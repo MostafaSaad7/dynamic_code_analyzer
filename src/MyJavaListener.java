3aeebec618ac4ee0c44d4eeb43e89589d6df5544
MayarHanafy <mayar.hanafy.14@gmail.com>
2022-04-26 22:49:53 +0200

    inject FileWriter in main

--- src/MyJavaListener.java
+++ src/MyJavaListener.java
@@ -6,9 +6,31 @@ public class MyJavaListener extends JavaParserBaseListener{
     TokenStreamRewriter rewriter;
 
     int blockNumber= 1;
+    String newClassName;
 
     public MyJavaListener(JavaParser parser, TokenStreamRewriter rewriter) {
         this.parser = parser; this.rewriter = rewriter;
+    }
+
+    @Override
+    public void enterMethodDeclaration(JavaParser.MethodDeclarationContext ctx) {
+        String methodName = ctx.identifier().getText();
+
+        if(methodName.equals("main"))
+        {
+            rewriter.insertAfter(ctx.methodBody().getStart(), "\n\n\t\tmyWriter = new FileWriter(\"visited_blocks/"+newClassName+"VisitedBlocks.txt\");\n");
+        }
+
+    }
+
+    @Override
+    public void exitMethodDeclaration(JavaParser.MethodDeclarationContext ctx) {
+        String methodName = ctx.identifier().getText();
+
+        if(methodName.equals("main"))
+        {
+            rewriter.insertBefore(ctx.methodBody().getStop(), "\n\n\t\tmyWriter.close();\n");
+        }
     }
 
 
