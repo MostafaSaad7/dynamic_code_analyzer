bf126c4cffa8988a8fc77a1bc1939224186dea42
Mostafa Ayman <mostafa.aymanae@gmail.com>
2022-04-26 22:41:45 +0200

    enterBlock override

--- augmented_files/augmented.java
+++ augmented_files/augmented.java
@@ -3,6 +3,10 @@
 class HelloWorld {
 	
     public static void main(String[] args) {
+
+		// block number: 1
+		myWriter.write("Entered Block Number: 1\n");
+
 		
         System.out.println("Hello, World!"); 
 		
--- src/MyJavaListener.java
+++ src/MyJavaListener.java
@@ -5,8 +5,20 @@ public class MyJavaListener extends JavaParserBaseListener{
     JavaParser parser;
     TokenStreamRewriter rewriter;
 
+    int blockNumber= 1;
+
     public MyJavaListener(JavaParser parser, TokenStreamRewriter rewriter) {
         this.parser = parser; this.rewriter = rewriter;
+    }
+
+
+    @Override
+    public void enterBlock(JavaParser.BlockContext ctx) {
+
+        // inject file write code
+        String insertCode = "// block number: "+blockNumber+"\n\t\tmyWriter.write(\"Entered Block Number: "+ blockNumber++ +"\\n\");";
+
+        rewriter.insertAfter(ctx.getStart(), "\n\n\t\t" + insertCode + "\n");
     }
 
 
